54e11d672934ed57bdd69f5a1282d958fc24cbcb
Jeremy Ebler <jebler@gmail.com>
2013-01-15 18:46:04 -0800

    initial commit

---
+++ lib/stasis-compass.rb
@@ -0,0 +1,7 @@
+require "stasis-compass/version"
+
+module Stasis
+  module Compass
+    # Your code goes here...
+  end
+end
---
+++ lib/stasis-compass/version.rb
@@ -0,0 +1,5 @@
+module Stasis
+  module Compass
+    VERSION = "0.0.1"
+  end
+end
